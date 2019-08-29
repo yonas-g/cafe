bd72edc78bc1d03b80e9fd3de504631e946873bb
yonas-g <yonasgebeyaw1990@gmail.com>
2019-08-29 14:26:14 +0300

    morgan removed for heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const logger = require('morgan')
 const app = require('express')()
 
 const orderApi = require('./api/orderApi')
@@ -9,8 +8,6 @@ const orderApi = require('./api/orderApi')
 const PORT = process.env.PORT || 5000
 const uri = process.env.URI
 const database = process.env.DATABASE_NAME
-
-app.use(logger('dev'))
 
 /** Mongoose */
 let url = uri + database
