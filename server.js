20cdb249f32cf4a4ecc6cda1ec3c714b82391409
yonas-g <yonasgebeyaw1990@gmail.com>
2019-08-29 15:38:36 +0300

    dev config setup

--- server.js
+++ server.js
@@ -7,20 +7,21 @@ const orderApi = require('./api/orderApi')
 /** Config */
 const PORT = process.env.PORT || 5000
 const env = process.env.NODE_ENV
-const uri = process.env.URI
-const database = process.env.DATABASE_NAME
+let uri = process.env.URI
+let database = process.env.DATABASE_NAME
 
 if (env == 'development') {
     let logger = require('morgan')
     uri = process.env.URI_TEST
-    database = DATABASE_NAME_TEST
+    database = process.env.DATABASE_NAME_TEST
 
-    app.user(logger('dev'))
+    console.log(uri + database);
+
+    app.use(logger('dev'))
 }
 
 /** Mongoose */
 let url = uri + database
-let test_url = process.env.URI_TEST + '/test'
 
 mongoose.connect(url, { useNewUrlParser: true }, err => {
     err ? console.log('Error Connecting to DB', err) : console.log('DB Connected');
