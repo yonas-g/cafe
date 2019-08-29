4ec3ae693424204c9a55053eba8ec6f78e6b7589
yonas-g <yonasgebeyaw1990@gmail.com>
2019-08-29 14:27:54 +0300

    gitignore remove /api

---
+++ api/orderApi.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+
+const Order = require('../model/order').Order
+const Available = require('../model/available').Available
+
+router.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        message: 'AVAILABLE_ORDERS'
+    })
+})
+
+router.post('/', (req, res) => {
+
+    let { from, orders } = req.body
+
+    let order = new Order({
+        from: from,
+        orders: orders.map(order => {
+            return {
+                name: new Available({
+                    name: order.name
+                }),
+                amount: order.amount
+            }
+        })
+    })
+
+    order.save((err, order) => {
+        err ? res.json({
+            ok: false,
+            error: err
+        }).status(500) : res.json({
+            ok: true,
+            order: order
+        }).status(200)
+    })
+})
+
+
+module.exports = router
