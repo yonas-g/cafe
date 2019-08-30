f8ec7d84fb69972a439686f168f9b6a7059044af
yonas-g <yonasgebeyaw1990@gmail.com>
2019-08-30 14:07:03 +0300

    available items CRUD

--- api/availableApi.js
+++ api/availableApi.js
@@ -22,11 +22,54 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    res.json(req.body)
+    if (!req.body.name) {
+        res
+            .status(400)
+            .json({
+                ok: false,
+                message: 'NAME_FIELD_NOT_FOUND'
+            })
+    } else {
+        AvailableItems.create({
+            name: req.body.name
+        }, (err, item) => {
+            err
+                ? res
+                    .status(500)
+                    .json({
+                        ok: false,
+                        message: 'UNABLE_TO_ADD_ITEM',
+                        error: err
+                    })
+                : res
+                    .status(200)
+                    .json({
+                        ok: true,
+                        message: 'ITEM_ADDED',
+                        data: item
+                    })
+        })
+    }
 })
 
 router.delete('/', (req, res) => {
-
+    AvailableItems.remove({ name: req.body.name }, (err, any) => {
+        err
+            ? res
+                .status(400)
+                .json({
+                    ok: false,
+                    message: 'UNABLE_TO_DELETE_ITEM',
+                    error: err
+                })
+            : res
+                .status(200)
+                .json({
+                    ok: true,
+                    message: 'ITEM_DELETED',
+                    data: any
+                })
+    })
 })
 
 module.exports = router
