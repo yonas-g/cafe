fe325c179d19efb431e080708aa28c4829ad8544
yonas-g <yonasgebeyaw1990@gmail.com>
2019-09-02 13:52:05 +0300

    availableSchema for order->name removed

--- model/order.js
+++ model/order.js
@@ -9,7 +9,6 @@ const OrderSchema = new mongoose.Schema({
     orders: [
         {
             name: {
-                type: AvailableSchema,
                 required: true
             },
             amount: {
